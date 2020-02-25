514e0fb0cfe670846a70efd1b953b9caa78f70a8
Victoria Blanchard <vlb19@imperial.ac.uk>
2020-02-25 23:40:55 +0000

    Added annotations to data analysis

--- MiniProject/code/DataAnalysis.py
+++ MiniProject/code/DataAnalysis.py
@@ -13,7 +13,19 @@ import matplotlib.pyplot as plt
 import matplotlib.axes as ax
 from matplotlib.patches import Polygon
 
+#########################################################
+### Load in data sets ###
+#########################################################
+
+# Data formatted for  ANOVA analysis
+analysisdata = pd.read_csv('../data/AnalysisTable.csv')
+
+# Data formatted for plotting
 graphdata = pd.read_csv("../data/OptimisedFitSummary.csv")
+
+#########################################################
+### Initial data visualisation ###
+#########################################################
 
 # Print number of columns loaded
 print("Loaded {} columns.".format(len(graphdata.columns.values)))
@@ -24,6 +36,11 @@ print(graphdata.columns.values)
 HabitatCrosstab = pd.crosstab(graphdata['BestModelAIC'], graphdata['Habitat'], margins = False)
 print(HabitatCrosstab)
 
+#########################################################
+### Generate box and whisker plot for habitats ###
+#########################################################
+
+# Crerate custom palettes
 sns.palplot(sns.diverging_palette(128, 240, n=4))
 custompalette = sns.diverging_palette(128, 240, n=4)
 
@@ -43,6 +60,10 @@ plt.legend(loc = 'upper right')
 # Save the figure to the results folder
 plt.savefig('../results/HabitatCompare')
 
+#########################################################
+### Generate violin plot for habitats ###
+#########################################################
+
 # Set dimensions of figure
 plt.figure(num=None, figsize=(20,10), dpi = 80, facecolor = 'w', edgecolor = 'w')
 
@@ -55,8 +76,16 @@ plt.ylabel("Minimum AIC Value")
 # Save the figure to the results folder
 plt.savefig('../results/HabitatCompareViolin')
 
+#########################################################
+### Visualise habitat data ###
+#########################################################
+
 Habitatcrosstab = pd.crosstab(graphdata['Habitat'], graphdata['MecOrPhen'], margins = False)
 print(Habitatcrosstab)
+
+################################################################
+### Generate bar chart for comparing model types per habitat ###
+################################################################
 
 # Set dimensions of figure
 plt.figure(num=None, figsize=(7,6), dpi = 80, facecolor = 'w', edgecolor = 'w')
@@ -70,7 +99,9 @@ plt.ylabel("Frequency of best model selected")
 # Save the figure to the results folder
 plt.savefig('../results/PhenOrMec')
 
-analysisdata = pd.read_csv('../data/AnalysisTable.csv')
+#########################################################
+### Visualise habitat analysis data ###
+#########################################################
 
 # Print number of columns loaded
 print("Loaded {} columns.".format(len(analysisdata.columns.values)))
@@ -78,18 +109,24 @@ print("Loaded {} columns.".format(len(analysisdata.columns.values)))
 # Print column headings
 print(analysisdata.columns.values)
 
+# Print top ten rows of dataframe
 analysisdata.head()
 
+# Run a two-way ANOVA on model AICs with Habitat and Model as factors
 HabitatANOVA = pg.anova(dv='AIC', between=['Habitat','Model'], data = analysisdata, detailed = True)
 HabitatANOVA
 
+# Run a pairwise Tukey HSD to compare differences between each habitat
 Habitattukey = pg.pairwise_tukey(data = analysisdata, dv = 'AIC', between=['Habitat'])
 Habitattukey
 
-
+# Run a pairwise Tukey HSD to compare differences between each model
 Modeltukey = pg.pairwise_tukey(data = analysisdata, dv = 'AIC', between=['Model'])
 Modeltukey
 
+#########################################################
+### Visualise consumer dimension analysis data ###
+#########################################################
 
 ConDimcrosstab = pd.crosstab(graphdata['BestModelAIC'], graphdata['Con_MovementDimensionality'], margins = False)
 print(ConDimcrosstab) 
